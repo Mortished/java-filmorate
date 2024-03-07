29c9274c66a140588bff805e3b30bb51922f6aa6
kakasy <drum2to@yandex.ru>
2024-03-07 16:26:47 +0300

    refactor: поправить checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageImplTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmDbStorageImplTest.java
@@ -18,7 +18,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
